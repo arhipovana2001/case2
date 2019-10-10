9d1095a958fc47f1dffbe1715852a164f2940714
arhipovana2001 <arhipovana2001@mail.ru>
2019-10-10 12:37:28 +0700

    start

---
+++ Main.py
@@ -0,0 +1,12 @@
+# Определяем социальную группу.
+print ("""Выбирете Ваш социальный статуc:
+1 - один субъект
+2 - супруг
+3 - родитель-одиночка""")
+social_status = input()
+if social_status == 1:
+    #TODO:
+elif social_status == 2:
+    #TODO:
+elif social_status == 3:
+    #TODO:
